d9931d819e100347dca81bdc5bff835e078d7010
eldani13 <ll-luis123@hotmail.com>
2025-02-19 14:51:29 -0500

    Nav

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex h-screen">
       {/* Navbar a la izquierda */}
-      <div className="w-64 bg-gray-800 text-white p-4">
+      <div className="w-80 bg-gray-900 text-white p-4">
         <NavBarComponent />
       </div>
       
