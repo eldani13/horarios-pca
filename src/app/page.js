a38084fbb18e18d0efde8468d233dd68873e1253
JuliannnR <juliroblesfonta07@gmail.com>
2025-02-22 10:02:43 -0500

    Adelanto1

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import NavBarComponent from "@/components/navbar/page";
 import ViewButtons from "../components/button/SelectView";
